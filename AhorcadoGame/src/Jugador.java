dd1a482e3dd6650f893458eaf25a978bd1c21c5d
uoome <nicogomezwp@gmail.com>
2019-07-22 19:08:36 -0300

    Update Jugador y Partida

--- AhorcadoGame/src/Jugador.java
+++ AhorcadoGame/src/Jugador.java
@@ -1,4 +1,44 @@
+import java.util.ArrayList;
 
 public class Jugador {
+	
+	// Variables
+	private String namePlayer;
+	private int id_player;
+	private ArrayList<Partida> playerGames;
+
+	// Getters and Setters
+    public String getNamePlayer() {
+		return namePlayer;
+	}
+
+	public void setNamePlayer(String namePlayer) {
+		this.namePlayer = namePlayer;
+	}
+
+	public int getId_player() {
+		return id_player;
+	}
+
+	public void setId_player(int id_player) {
+		this.id_player = id_player;
+	}
+
+	public ArrayList<Partida> getPlayerGames() {
+		return playerGames;
+	}
+
+	public void setPlayerGames(ArrayList<Partida> playerGames) {
+		this.playerGames = playerGames;
+	}
+
+	// Constructor
+	public Jugador(){
+        this.setPlayerGames(new ArrayList<Partida>());
+    }
+	
+	// Methods
+    
+    
 
 }
--- AhorcadoGame/src/Partida.java
+++ AhorcadoGame/src/Partida.java
@@ -1,4 +1,150 @@
 
 public class Partida {
+	
+	// Variables
+	private int score, guessedLetters, unguessedLetters, lifes;
+	private String wordToGuess;
+	private String[] alphabet = {"perro","arbol","manzana","kanvan","adrenalina","objeto"};
+	private long startTime, endTime, gameDuration;
+	
+	// Getters and Setters
+	public int getScore() {
+		return score;
+	}
+
+	public void setScore(int score) {
+		this.score = score;
+	}
+
+	public int getGuessedLetters() {
+		return guessedLetters;
+	}
+
+	public void setGuessedLetters(int guessedLetters) {
+		this.guessedLetters = guessedLetters;
+	}
+
+	public int getUnguessedLetters() {
+		return unguessedLetters;
+	}
+
+	public void setUnguessedLetters(int unguessedLetters) {
+		this.unguessedLetters = unguessedLetters;
+	}
+
+	public int getLifes() {
+		return lifes;
+	}
+
+	public void setLifes(int lifes) {
+		this.lifes = lifes;
+	}
+
+	public String getWordToGuess() {
+		return wordToGuess;
+	}
+
+	public void setWordToGuess(String wordToGuess) {
+		this.wordToGuess = wordToGuess;
+	}
+
+	public String[] getAlphabet() {
+		return alphabet;
+	}
+
+	public void setAlphabet(String[] alphabet) {
+		this.alphabet = alphabet;
+	}
+
+	public long getStartTime() {
+		return startTime;
+	}
+
+	public void setStartTime(long startTime) {
+		this.startTime = startTime;
+	}
+
+	public long getEndTime() {
+		return endTime;
+	}
+
+	public void setEndTime(long endTime) {
+		this.endTime = endTime;
+	}
+
+	public long getGameDuration() {
+		return gameDuration;
+	}
+
+	public void setGameDuration(long gameDuration) {
+		this.gameDuration = gameDuration;
+	}
+
+	// Constructor
+	public Partida() {
+		this.setLifes(5);
+		this.setWordToGuess(this.generateWord());
+		this.setGuessedLetters(0);
+		this.setUnguessedLetters(0);
+		this.setScore(0);
+		this.setStartTime(System.currentTimeMillis());
+	}
+	
+	// Methods
+	public String generateWord() {
+		int index = (int) (Math.random() * this.getAlphabet().length);
+		return alphabet[index];
+	}
+	
+	public void calculateScore() {
+		this.setScore(this.getGuessedLetters() * 2 - this.getUnguessedLetters()); 
+		System.out.println("Su score es: "  + this.getScore());	
+	}
+
+	public void calculateGameTime() {
+		this.setGameDuration(this.getEndTime() - this.getStartTime());    
+		System.out.println("Duracion (en milisegundos): " + this.getGameDuration());
+	}
+	
+	public int contarCaracteresRepetidos(String caracter) {
+        int posicion, contador = 0;
+        // Busco la primera vez que aparece
+        posicion = this.getWordToGuess().indexOf(caracter);
+        System.out.println("La letra se encuentra en la posicion: " + posicion);
+        while (posicion != -1) { //mientras se encuentre el caracter
+            contador++; //se cuenta la repeticion
+            //busco a partir de la posición siguiente a la encontrada
+            posicion = this.getWordToGuess().indexOf(caracter, posicion + 1);
+            if(posicion != -1)
+            	System.out.println("La letra se encuentra en la posicion: " + posicion);
+        }
+        return contador;
+	}
+	
+	// Verificar si va aca, quizas deba ir en el servlet
+	/*public void adivinarLetra(String letra) {
+		if(letra.isEmpty()) {
+			System.out.println("Ingrese alguna letra");
+			this.vidas --;
+			System.out.println("Vidas restantes: " + this.getVidas());
+			
+		} else if(this.palabraAdivinar.contains(letra)) {
+			System.out.println("El caracter '" + letra + "' es correcto");
+			this.cantLetrasAdivinadas += this.contarCaracteresRepetidos(letra); 
+			
+		} else {
+			System.out.println("El caracter '" + letra + "' es erroneo");
+			this.cantLetrasErradas ++;
+			this.vidas --;
+			System.out.println("Vidas restantes: " + this.getVidas());
+		}	
+	}*/
+
+	
+	
+	
+	
+	
+	
 
 }
